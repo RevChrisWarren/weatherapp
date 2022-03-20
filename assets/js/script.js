b9a9522c6bc9e2e478a95bd2a68aa160c4b52807
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-03-20 16:02:46 -0500

    Working on dynamically added buttons functions and storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var forecastContainerEl = document.querySelector("#weatherdata");
 var currentWeatherContainer = document.getElementById("current");
 var forecastWeatherContainer = document.getElementById("forecast");
 var fiveDayTitle = document.getElementById("five-day");
+var btn2El = document.querySelector("#button2");
+
 
 var formSubmitHandler = function (event) {
     //prevent page from refreshing
@@ -20,7 +22,7 @@ var formSubmitHandler = function (event) {
 
         //clear old content
         forecastContainerEl = "";
-        cityEl.value = "";
+        //cityEl.value = "";
     } else {
         alert("Please enter a city name")
     }
@@ -66,6 +68,12 @@ function getWeatherData(latLon) {
         .then(function (forecastData) {
             // Current Weather Data
             console.log(forecastData);
+            var currentIcon = forecastData.current.weather[0].icon;
+            console.log(currentIcon);
+            var currentIconUrl = `<img src="http://openweathermap.org/img/wn/${currentIcon}@2x.png">`;
+            var currentIconEl = document.createElement("p");
+            currentIconEl.innerHTML = currentIconUrl;
+            currentWeatherContainer.append(currentIconEl);
             //calculate kelvin temperature into fahrenheit (0K − 273.15) × 9/5 + 32 = -459.7°F
             var temp = ((forecastData.current.temp - 273.15) * 9 / 5 + 32).toFixed(2);
             console.log('temp: ', temp);
@@ -81,17 +89,25 @@ function getWeatherData(latLon) {
             humidityEl.textContent = `Humidity: ${humidity} %`;
             var UV = forecastData.current.uvi;
             console.log('UV: ', UV);
-            var UVEl = document.createElement("p");
-            UVEl.textContent = `UV Index: ${UV}`;
+            var UVIntroEl = document.createElement("p");
+            UVIntroEl.textContent = "UV Index: ";
+            var UVEl = document.createElement("div");    
+            UVEl.textContent =" " + UV + " ";
+            if (UV <= 2) {
+                UVEl.setAttribute("style", "background-color: rgb(139, 230, 124); width: 35px");
+            } else if (UV > 2) {
+                UVEl.setAttribute("style", "background-color: rgb(240, 161, 150); width: 35px");
+            }
             currentWeatherContainer.setAttribute("style", "border: 2px solid black");
-            currentWeatherContainer.append(tempEl, windEl, humidityEl, UVEl);
+            currentWeatherContainer.append(tempEl, windEl, humidityEl, UVIntroEl, UVEl);
 
             // Forecast Weather Data
 
             var forecastDailyArray = forecastData.daily
             var title = document.createElement("h2");
-                title.textContent = "5-Day Forecast:"
-                fiveDayTitle.append(title);
+                title.innerHTML = "5-Day Forecast:" + "</br>";
+                title.style.width= "100%";
+                forecastWeatherContainer.append(title);
             forecastDailyArray.slice(0, 5).forEach(function (dailyData) {
                 
                 var container = document.createElement("div");
@@ -124,6 +140,22 @@ function getWeatherData(latLon) {
         })
 }
 
+function btnCreator() {
+    var cityButton = document.createElement("button");
+    cityButton.type = "button";
+    cityButton.innerHTML = (cityEl.value);
+    cityButton.className = "btn btn-secondary"
+    btn2El.append(cityButton);
+    cityButton.onClick = function() {
+        alert("boo");
+     
+    }
+}
+
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+userFormEl.addEventListener("submit", btnCreator);
+
+
 
 
