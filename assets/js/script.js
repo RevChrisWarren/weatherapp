83f1cb9ae5098c097772208d8e4505b466223b60
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-03-22 10:48:42 -0500

    working on adding elements to an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,20 +14,24 @@ var formSubmitHandler = function (event) {
 
     //get value from input element
     var cityName = cityEl.value.trim();
+    console.log('cityName: ', cityName);
+    cityArray.push(cityName);
     if (cityName) {
         getCityGeo(cityName)
             .then(function (latLon) {
-                console.log('latLon: ', latLon);
+                //console.log('latLon: ', latLon);
                 return getWeatherData(latLon)
             })
 
         //clear old content
         forecastContainerEl = "";
-        //cityEl.value = "";
+        cityEl.value = "";
     } else {
         alert("Please enter a city name")
     }
 };
+console.log(cityArray);
+
 
 function getCityGeo(city) {
     //find the latitude and longitude for the city
@@ -41,7 +45,7 @@ function getCityGeo(city) {
             }
         })
         .then(function (data) {
-            console.log('data: ', data);
+            //console.log('data: ', data);
             var currentCity = data[0].name;
             var todayDate = moment().format("MM/DD/YYYY");
             var currentCityDateEl = document.createElement("h2")
@@ -61,38 +65,38 @@ function getWeatherData(latLon) {
     var lat = latLon.lat;
     var lon = latLon.lon;
     var forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=8d03dfd7dbf3df23ffe6a5d84a5e5242`
-    console.log('forecastUrl: ', forecastUrl);
+    //console.log('forecastUrl: ', forecastUrl);
     fetch(forecastUrl)
         .then(function (res) {
             return res.json()
         })
         .then(function (forecastData) {
             // Current Weather Data
-            console.log(forecastData);
+           // console.log(forecastData);
             var currentIcon = forecastData.current.weather[0].icon;
-            console.log(currentIcon);
+            //console.log(currentIcon);
             var currentIconUrl = `<img src="http://openweathermap.org/img/wn/${currentIcon}@2x.png">`;
             var currentIconEl = document.createElement("p");
             currentIconEl.innerHTML = currentIconUrl;
             currentWeatherContainer.append(currentIconEl);
             //calculate kelvin temperature into fahrenheit (0K − 273.15) × 9/5 + 32 = -459.7°F
             var temp = ((forecastData.current.temp - 273.15) * 9 / 5 + 32).toFixed(2);
-            console.log('temp: ', temp);
+            //console.log('temp: ', temp);
             var tempEl = document.createElement("p");
             tempEl.textContent = `Temp: ${temp} ℉`;
             var wind = forecastData.current.wind_speed;
-            console.log('wind: ', wind);
+           // console.log('wind: ', wind);
             var windEl = document.createElement("p");
             windEl.textContent = `Wind: ${wind} MPH`;
             var humidity = forecastData.current.humidity;
-            console.log('humidity: ', humidity);
+            //console.log('humidity: ', humidity);
             var humidityEl = document.createElement("p");
             humidityEl.textContent = `Humidity: ${humidity} %`;
             var UV = forecastData.current.uvi;
-            console.log('UV: ', UV);
+           // console.log('UV: ', UV);
             var UVIntroEl = document.createElement("p");
             UVIntroEl.textContent = "UV Index: ";
-            var UVEl = document.createElement("div");    
+            var UVEl = document.createElement("p");  
             UVEl.textContent =" " + UV + " ";
             if (UV <= 2) {
                 UVEl.setAttribute("style", "background-color: rgb(139, 230, 124); width: 35px");
@@ -101,6 +105,7 @@ function getWeatherData(latLon) {
             }
             currentWeatherContainer.setAttribute("style", "border: 2px solid black");
             currentWeatherContainer.append(tempEl, windEl, humidityEl, UVIntroEl, UVEl);
+            
 
             // Forecast Weather Data
 
@@ -145,17 +150,16 @@ function btnCreator() {
     cityButton.type = "button";
     cityButton.innerHTML = (cityEl.value);
     cityButton.className = "btn btn-secondary"
-       btn2El.append(cityButton);
-    cityButton.onClick = function() {
-        alert("boo");
-     
+       btn2El.append(cityButton);     
     }
-}
+
+    // function cityButtonPress() {
+    //     makeCityArray();
+    // }
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 userFormEl.addEventListener("submit", btnCreator);
-
-btn2El.addEventListener("click", alert("boo"));
+// userFormEl.addEventListener("submit", cityButtonPress);
 
 
